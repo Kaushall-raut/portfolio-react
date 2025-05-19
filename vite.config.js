808f04db3e930cb65ab0947847bf0024212dc320
Kaushall-raut <KaushalRaut.code@gmail.com>
2025-05-19 18:03:44 +0530

    remove files

--- vite.config.js
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from "vite";
-import tailwindcss from "@tailwindcss/vite";
-import viteReact from "@vitejs/plugin-react";
-
-export default defineConfig({
-  plugins: [tailwindcss(), viteReact()],
-});
